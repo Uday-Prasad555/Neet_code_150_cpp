ec5ad8392a0e7ddabfb15738c619e85e7fca3163
Uday Prasad <udayprasad174396@gmail.com>
2026-08-25 23:50:39 +0530

    25-08-26

--- BinarySearch/kokoEatingBanana.cpp
+++ BinarySearch/kokoEatingBanana.cpp
@@ -1,24 +1,30 @@
-class Solution {
+class Solution
+{
 public:
-    int minEatingSpeed(vector<int>& piles, int h) {
+    int minEatingSpeed(vector<int> &piles, int h)
+    {
         int r = *max_element(piles.begin(), piles.end());
         int l = 1;
         long long a;
         int mid;
         int ans = r;
 
-        while (l <= r) {
+        while (l <= r)
+        {
             mid = l + (r - l) / 2;
             a = 0;
-            for (int i = 0; i < piles.size(); i++) {
+            for (int i = 0; i < piles.size(); i++)
+            {
                 a += (piles[i] + mid - 1) / mid;
             }
 
-            if (a <= h) {
+            if (a <= h)
+            {
                 ans = mid;
                 r = mid - 1;
             }
-            else if (a > h) {
+            else if (a > h)
+            {
                 l = mid + 1;
             }
         }
---
+++ other problems/3718.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int missingMultiple(vector<int> &nums, int k)
+    {
+        sort(nums.begin(), nums.end());
+        int m = 1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == k * m)
+            {
+                m = m + 1;
+            }
+        }
+        return k * m;
+    }
+};
